bd2118c5b67ec9d28084f392c63a94e1eb3c4c89
Adrian Suaza <adri_sm01@hotmail.com>
2019-12-20 14:06:55 +0100

    Se documenta la clase Rectangulo para el javadoc y se corrigen errores

--- src/FiguraPlana/TestFiguraPlana.java
+++ src/FiguraPlana/TestFiguraPlana.java
@@ -1,10 +1,18 @@
 package FiguraPlana;
 import java.io.*;
 /**
- * Esta Clase testea el objeto FiguraPlana y sus 
- * */
+*  Esta Clase testea el objeto FiguraPlana y los objetos Rectangulo y Triangulo
+*  
+* @author: Adrian Suaza
+* @version: 20/12/2019/A
+* @see <a href = "https://aulavirtual33.educa.madrid.org/ies.goya.madrid/pluginfile.php/10924/mod_resource/content/0/JavaDoc.pdf" /> Documentacion de Javadoc </a>
+*/
 class TestFiguraPlana
 {
+	/**
+	 * Metodo main(principal) crea cada objeto y se le asignan unos datos
+	 * el metodo devuelve en pantalla el area de cada figura.
+	 * */
 	public static void main(String args[])
 	{
 		Rectangulo r =new Rectangulo(8,5);
